a0ebd9e8d8375ef1b5df3600fd3a773cda136363
Muhammad Shaker <mada.tmw@gmail.com>
2023-03-06 01:14:38 +0200

    part1, exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,7 +2,16 @@ const Header = (props) =>{
   return <h1>{props.course}</h1>
 }
 const Content = (props) =>{
-  return <p>{props.part} {props.exercises}</p>
+  return (
+    <>
+      <Part name={props.name1} exercises={props.exercises1}/>
+      <Part name={props.name2} exercises={props.exercises2}/>
+      <Part name={props.name3} exercises={props.exercises3}/>
+    </>
+  )
+}
+const Part = (props) =>{
+  return <p>{props.name} {props.exercises}</p>
 }
 const Total = (props) =>{
   return <p>Number of exercises {props.totalExercises}</p>
@@ -19,9 +28,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content name1={part1} exercises1={exercises1} name2={part2} exercises2={exercises2} name3={part3} exercises3={exercises3}/>
       <Total totalExercises={exercises1+exercises2+exercises3}/>
     </div>
   )
